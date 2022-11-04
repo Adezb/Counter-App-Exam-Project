4a47cd31d497f7bfbfa5474149e7baf3d53f2745
Adezb <temitopeceo@gmail.com>
2022-11-04 04:08:02 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
+
+import CounterOne from "./pages/CounterOne";
+import CounterTwo from "./pages/CounterTwo";
+import ErrorPage from "./pages/ErrorPage";
+import ErrorBoundTest from "./pages/ErrorBoundTest";
+import MyNavLink from "./customhook/MyNavLink";
+import Fallback from "./components/Fallback";
+import "./counterone.css";
+import "./countertwo.css";
+
+function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <MyNavLink to="/" className="nav-link">
+        Counter One
+      </MyNavLink>
+      <MyNavLink to="/countertwo" className="nav-link">
+        Counter Two
+      </MyNavLink>
+      <MyNavLink to="/errorboundtest" className="nav-link">
+        ErrorBound
+      </MyNavLink>
+    </nav>
+  );
+}
 
 function App() {
+  const errorHandler = (error, errorInfo) => {
+    console.log("Logging", error, errorInfo);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ErrorBoundary FallbackComponent={Fallback} onError={errorHandler}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<CounterOne />} />
+          <Route path="/countertwo" element={<CounterTwo />} />
+          <Route path="/errorboundtest" element={<ErrorBoundTest />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </ErrorBoundary>
     </div>
   );
 }
