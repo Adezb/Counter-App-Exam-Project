b8e29d795da21559f0a99e09285dc0fc3f375fd0
Adezb <temitopeceo@gmail.com>
2022-11-04 22:57:00 +0100

    Updated

--- src/pages/CounterTwo.js
+++ src/pages/CounterTwo.js
@@ -5,7 +5,7 @@ const CounterTwo = () => {
   const { count, increment, decrement, setValue, reset } = UseReducerCounter(0);
 
   return (
-    <div className="wrapper-two">
+    <>
       <Helmet>
         <title>Counter Two</title>
         <meta
@@ -14,31 +14,37 @@ const CounterTwo = () => {
         />
       </Helmet>
 
-      <div className="counter-wrapper">
-        <div className="counter-title">Counter Two </div>
-        <p className="counter-description">UseReducer Counter</p>
-
-        <div className="counter-box">{count}</div>
+      <div className="error-link">
+        Go to <Link to="/error">Error 404 Page</Link>
       </div>
 
-      <div className="counter-btn-wrapper">
-        <button className="counter-two-btn" onClick={increment}>
-          Increment +
-        </button>
-        <button className="counter-two-btn" onClick={setValue}>
-          Set Value
-        </button>
-        <button className="counter-two-btn" onClick={decrement}>
-          Decrement -
-        </button>
-      </div>
+      <div className="wrapper-two">
+        <div className="counter-wrapper">
+          <div className="counter-title">Counter Two </div>
+          <p className="counter-description">UseReducer Counter</p>
 
-      <div className="reset-value">
-        <button className="reset-btn" onClick={reset}>
-          Reset
-        </button>
+          <div className="counter-box">{count}</div>
+        </div>
+
+        <div className="counter-btn-wrapper">
+          <button className="counter-two-btn" onClick={increment}>
+            Increment +
+          </button>
+          <button className="counter-two-btn" onClick={setValue}>
+            Set Value
+          </button>
+          <button className="counter-two-btn" onClick={decrement}>
+            Decrement -
+          </button>
+        </div>
+
+        <div className="reset-value">
+          <button className="reset-btn" onClick={reset}>
+            Reset
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
