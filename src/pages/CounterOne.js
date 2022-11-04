7625d5edf2b0bd9cfc574a146de225c756809cb8
Adezb <temitopeceo@gmail.com>
2022-11-04 09:40:42 +0100

    Updated

--- src/pages/CounterOne.js
+++ src/pages/CounterOne.js
@@ -7,7 +7,7 @@ const CounterOne = () => {
   const { count, increment, decrement, setValue, reset } = MyCounter(0);
 
   return (
-    <div className="wrapper-one">
+    <>
       <Helmet>
         <title>Counter One</title>
         <meta
@@ -19,31 +19,32 @@ const CounterOne = () => {
       <div className="error-link">
         Go to <Link to="/error">Error 404 Page</Link>
       </div>
+      <div className="wrapper-one">
+        <div className="counter-wrapper">
+          <div className="counter-title">Counter One</div>
+          <p className="counter-description">Custom Hook Counter</p>
+          <div className="counter-box">{count}</div>
+        </div>
 
-      <div className="counter-wrapper">
-        <div className="counter-title">Counter One</div>
-        <p className="counter-description">Custom Hook Counter</p>
-        <div className="counter-box">{count}</div>
-      </div>
+        <div className="counter-btn-wrapper">
+          <button className="counter-one-btn" onClick={increment}>
+            Increment +
+          </button>
+          <button className="counter-one-btn" onClick={setValue}>
+            Set Value
+          </button>
+          <button className="counter-one-btn" onClick={decrement}>
+            Decrement -
+          </button>
+        </div>
 
-      <div className="counter-btn-wrapper">
-        <button className="counter-one-btn" onClick={increment}>
-          Increment +
-        </button>
-        <button className="counter-one-btn" onClick={setValue}>
-          Set Value
-        </button>
-        <button className="counter-one-btn" onClick={decrement}>
-          Decrement -
-        </button>
+        <div className="reset-value">
+          <button className="reset-btn" onClick={reset}>
+            Reset
+          </button>
+        </div>
       </div>
-
-      <div className="reset-value">
-        <button className="reset-btn" onClick={reset}>
-          Reset
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
